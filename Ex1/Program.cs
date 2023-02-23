fa14ce95a1a8ebe06c08bd0a3d48cc829a270f7e
Kattebu <butskoev@tut.by>
2023-02-23 13:20:55 +0300

    ex4

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,12 +1,13 @@
 ﻿// Ex1
 Console.Write("введите число: ");
 var number = Convert.ToInt32(Console.ReadLine());
-if (number>=1000 && number<=99999)
+if (number>=10000 && number<=99999)
 {
     Console.WriteLine("число пятизначное");
     var s = number.ToString();
     for(int i=0;i<=s.Length/2;i++)
-    if (s[i]==s[s.Length-1-i])
+    {
+      if (s[i]==s[s.Length-1-i])
     {
     Console.WriteLine("число является палиндромом");
     break;
@@ -14,7 +15,7 @@ if (number>=1000 && number<=99999)
     else
     {
     Console.WriteLine("число не является палиндромом");
-    return;
+    }
     }
 }
 else
