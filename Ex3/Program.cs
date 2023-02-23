30fafd22a1e26d7734d6eeaffee9a5f710f92fb3
Kattebu <butskoev@tut.by>
2023-02-23 13:03:22 +0300

    ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Ex3
+Console.Write("введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i=1; i<=N; i++)//i+=2 = i=i+2
+{
+  Console.Write(Convert.ToInt32(Math.Pow(i,3)) +"\t");  
+}
