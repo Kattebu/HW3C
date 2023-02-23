3d70439204556a1e5ebee3e8b2e05bc03e24c274
Kattebu <butskoev@tut.by>
2023-02-23 13:07:02 +0300

    ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Ex4 доп
+Console.Write("введите сумму вклада: ");
+int deposite = Convert.ToInt32(Console.ReadLine());
+if (deposite<100)
+{
+    Console.WriteLine("Общая сумма: "+ Math.Round((deposite+deposite*0.05),2));
+}
+else if (deposite<200)
+{
+Console.WriteLine("Общая сумма: "+ Math.Round((deposite+deposite*0.07),2));
+}
+else
+{
+    Console.WriteLine("Общая сумма: "+ Math.Round((deposite+deposite*0.1),2));
+}
