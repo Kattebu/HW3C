28144e3d9b5e46ae21e1e61cfee1028d8823a5ed
Kattebu <butskoev@tut.by>
2023-02-23 13:21:16 +0300

    chan

---
+++ Ex5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Ex5 dop
+Console.Write("введите число: ");
+var number = Convert.ToInt32(Console.ReadLine());
+if (number>=100 && number<=99999)
+{
+    Console.WriteLine("число пятизначное");
+    var s = number.ToString();
+    for(int i=0;i<=s.Length/2;i++)
+    {
+      if (s[i]==s[s.Length-1-i])
+    {
+    Console.WriteLine("число является палиндромом");
+    break;
+    }
+    else
+    {
+    Console.WriteLine("число не является палиндромом");
+    }
+    }
+}
+else
+{
+  Console.WriteLine("число не пятизначное");  
+}
